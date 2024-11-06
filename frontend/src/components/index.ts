bbc4408c5b64bedc9c41b98377bb33123123db87
Mike <34812500+mikey-semy@users.noreply.github.com>
2024-11-06 22:10:59 +0300

    Update index.ts

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Header } from './Header';
+export { default as Header } from './header';
 export { default as Logo } from './Common/Logo';
 export { default as Form } from './Common/Form';
 export { default as Button } from './Common/Button';
